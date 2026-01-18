150bf49d1c456a50e89467f25434d37cf13d8fea
SyrHamza <modernhahahha@gmail.com>
2026-01-18 15:25:57 +0100

    Update manhwaread.js

--- manhwaread.js
+++ manhwaread.js
@@ -1,25 +1,50 @@
-const config = {
+const extension = {
     name: "ManhwaRead",
     baseUrl: "https://manhwaread.com",
     lang: "en",
-    type: "madara",
-    iconUrl: "https://manhwaread.com/favicon.ico"
+    nsfw: 1,
+
+    // Suche und Startseite
+    getLatestRequest: (page) => `${extension.baseUrl}/manga/page/${page}/?m_order=latest`,
+    getSearchRequest: (query, page) => `${extension.baseUrl}/page/${page}/?s=${query}&post_type=wp-manga`,
+
+    // Parser für die Liste (Nutzt deine Selektoren)
+    parseMangaList: (html) => {
+        const results = [];
+        const doc = new DOMParser().parseFromString(html, 'text/html');
+        doc.querySelectorAll('div.c-tabs-item__content').forEach(item => {
+            results.push({
+                name: item.querySelector('div.post-title h3 a').textContent.trim(),
+                url: item.querySelector('div.post-title h3 a').getAttribute('href'),
+                thumbnail: item.querySelector('img.img-responsive').getAttribute('src')
+            });
+        });
+        return results;
+    },
+
+    // Kapitel-Parser
+    parseChapterList: (html) => {
+        const chapters = [];
+        const doc = new DOMParser().parseFromString(html, 'text/html');
+        doc.querySelectorAll('li.wp-manga-chapter').forEach(ch => {
+            const link = ch.querySelector('a');
+            chapters.push({
+                name: link.textContent.trim(),
+                url: link.getAttribute('href')
+            });
+        });
+        return chapters;
+    },
+
+    // Bilder-Parser (für die Seiten im Kapitel)
+    parsePageList: (html) => {
+        const pages = [];
+        const doc = new DOMParser().parseFromString(html, 'text/html');
+        doc.querySelectorAll('div.page-break img').forEach(img => {
+            pages.push(img.getAttribute('src').trim());
+        });
+        return pages;
+    }
 };
 
-class ManhwaRead extends Madara {
-    constructor() {
-        super(config);
-    }
-
-    // Hier nutzen wir deine Selektoren aus dem Screenshot
-    get selectors() {
-        return {
-            mangaList: "div.c-tabs-item__content",
-            title: "div.post-title h3 a",
-            url: "div.post-title h3 a",
-            thumbnail: "img.img-responsive"
-        };
-    }
-}
-
-module.exports = new ManhwaRead();
+module.exports = extension;
